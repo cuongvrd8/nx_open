5a6a028af78bd5722b239fe5aecaa80a3a865be9
Yuriy <ynikitenkov@networkoptix.com>
2022-08-17 17:20:59 -0700

    MOBILE-1550: Fixed crash on reconnection

--- vms/libs/nx_vms_common/src/core/resource/camera_history.cpp
+++ vms/libs/nx_vms_common/src/core/resource/camera_history.cpp
@@ -253,6 +253,9 @@ QnCameraHistoryPool::StartResult QnCameraHistoryPool::updateCameraHistoryAsync(
     if (camera.isNull())
         return StartResult::failed;
 
+    if (!m_context->ec2Connection()) //< Reconnecting to the server after idle.
+        return StartResult::failed;
+
     if (isCameraHistoryValid(camera))
         return StartResult::ommited;
 
